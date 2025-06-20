82648fd4801e18e9667960cbadbc37b3cd1a1fdb
anton <spotifig2@gmail.com>
2025-06-20 23:51:28 +0500

    практика 5.1

---
+++ pract5/pract5/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pract5
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DateLabel.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            CurrencyDatePicker.Date = DateTime.Now;
+            OnCalculateClicked(null, null);
+        }
+        private void TermSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TermEntry.Text = ((int)e.NewValue).ToString();
+            OnCalculateClicked(null, null);
+        }
+        private void RateSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            OnCalculateClicked(null, null);
+        }
+        private void CurrencyDatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DateLabel.Text = e.NewDate.ToString("dd/MM/yyyy");
+        }
+        private void OnCalculateClicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(AmountEntry.Text) || Convert.ToInt32(AmountEntry.Text) == 0) return;
+            if (string.IsNullOrWhiteSpace(TermEntry.Text) || Convert.ToInt32(TermEntry.Text) == 0)
+            {
+                MonthlyPaymentLabel.Text = "...";
+                TotalPaymentLabel.Text = "...";
+                OverpaymentLabel.Text = "...";
+                return;
+            }
+            double amount, rate;
+            int term;
+            if (!double.TryParse(AmountEntry.Text, out amount)) return;
+            if (!int.TryParse(TermEntry.Text, out term)) return;
+            rate = RateSlider.Value;
+            PercentStavka.Text = $"Процентная ставка: {RateSlider.Value}";
+            bool isAnnuity = PaymentTypePicker.SelectedIndex == 0;
+            if (isAnnuity)
+            {
+                CalculateAnnuity(amount, term, rate);
+            }
+            else
+            {
+                CalculateDifferentiated(amount, term, rate);
+            }
+        }
+        private void CalculateAnnuity(double amount, int term, double rate)
+        {
+            double monthlyRate = rate / 100 / 12;
+            double coefficient = (monthlyRate * Math.Pow(1 + monthlyRate, term)) /
+                                (Math.Pow(1 + monthlyRate, term) - 1);
+            double monthlyPayment = amount * coefficient;
+            double totalPayment = monthlyPayment * term;
+            double overpayment = totalPayment - amount;
+            MonthlyPaymentLabel.Text = monthlyPayment.ToString("N2");
+            TotalPaymentLabel.Text = totalPayment.ToString("N2");
+            OverpaymentLabel.Text = overpayment.ToString("N2");
+        }
+        private void CalculateDifferentiated(double amount, int term, double rate)
+        {
+            double mainDebt = amount / term;
+            double totalPayment = 0;
+            double firstPayment = mainDebt + (amount * rate / 100 / 12);
+            totalPayment = (firstPayment + mainDebt + (mainDebt * rate / 100 / 12)) / 2 * term;
+            double overpayment = totalPayment - amount;
+            MonthlyPaymentLabel.Text = firstPayment.ToString("N2") + " ... " +
+                                      (mainDebt + (mainDebt * rate / 100 / 12)).ToString("N2");
+            TotalPaymentLabel.Text = totalPayment.ToString("N2");
+            OverpaymentLabel.Text = overpayment.ToString("N2");
+        }
+    }
+}
---
+++ pract5/pract5/WelcomePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pract5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSignInClicked(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(usernameEntry.Text) || string.IsNullOrWhiteSpace(passwordEntry.Text))
+            {
+                await DisplayAlert("Ошибка", "Пожалуйста, заполните все поля", "OK");
+                return;
+            }
+
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
